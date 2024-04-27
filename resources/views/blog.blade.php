9b373225f0a4e458473bf789a571f1b744b08b45
Chopper <riskidrian1@gmail.com>
2024-04-27 13:00:16 +0700

    feat: route, page, tailwind, alpinejs

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog page</title>
+</head>
+<body>
+    <h1>Blog Page</h1>
+    <article>
+        <h1>Title</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, quo!</p>
+    </article>
+    <article>
+        <h1>Title</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, quo!</p>
+    </article>
+</body>
+</html>
